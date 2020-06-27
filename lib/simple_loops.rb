8eb13dfe56deec7c74568b20a47d0f2fa25f01a3
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-27 12:32:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,7 +27,7 @@ def return_string_array(array)
   new_array = []
   
   while array[counter] do 
-    new_array.push(array[counter].to_s)
+    new_array << array[counter].to_s
     counter += 1
   end 
   new_array
