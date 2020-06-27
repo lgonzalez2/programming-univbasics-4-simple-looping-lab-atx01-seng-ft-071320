49a06183aeabd8aa210922e1f7231f94e6125f63
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-27 12:25:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,7 +12,13 @@ def loop_message_n_times(message, n)
 end
 
 
-def output_array(message)
+def output_array(array)
+  counter = 0  
+  
+  while array[counter] do 
+    puts array[counter]
+    counter += 1 
+  end 
 end
 
 
