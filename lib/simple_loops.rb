16d2ddd70b41e62f702c25d2beb090aba6e39f5d
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-27 12:22:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,3 +3,18 @@ def loop_message_five_times(message)
     puts message
   end
 end
+
+
+def loop_message_n_times(message, n)
+  n.times do
+    puts message
+  end
+end
+
+
+def output_array
+end
+
+
+def return_string_array
+end
